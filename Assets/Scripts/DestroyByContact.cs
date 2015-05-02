15212da21de0376889afe08ab1a53af1dc3c08b3
Sara B <sara.alexia.b@gmail.com>
2015-05-01 23:06:50 -0400

    Created explosions (asteroid by bolt or player).

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+	public GameObject explosion;
+	public GameObject PlayerExplosion;
 
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.tag == "Boundary")
-		{
+		if (other.tag == "Boundary") {
 			return;
+		}
+
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (PlayerExplosion, other.transform.position, other.transform.rotation);
 		}
 
 		Destroy(other.gameObject);
