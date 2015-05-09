2e9272ecf7c2275439261e7cd283735a628e4702
Sara B <sara.alexia.b@gmail.com>
2015-05-09 08:53:54 -0400

    Fixed bugs, added comments on PlayerController script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * This script defines the playble area, gets the player moving, tilting and shooting.
+ * 
+ */
+
+// 
+// Seralizes the class Boundary to be visible and edited in the inspector,
+// as it is not recognized by Unity.
 [System.Serializable]
 
+// Boundary class to define the playable area
 public class Boundary
 {
 	public float xMin, xMax, zMin, zMax;
@@ -10,18 +19,27 @@ public class Boundary
 
 public class PlayerController : MonoBehaviour
 {
+	// player movement variables
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
 
+	// shot variables
 	public GameObject shot;
 	public Transform shotSpawn;
 	public float fireRate;
-
 	public float nextFire;
 
+	// Called every frame
 	void Update ()
 	{
+		/**
+		 * Condition for shooting: if the button is pushed, a shot is instantiated at the
+		 * determined position and triggers the sound associated with the shot.
+		 * Also allows us to set the fire rate via the inspector.
+		 * 
+		 */
+
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
@@ -29,8 +47,11 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
+	// Called every fixed framerate frame
 	void FixedUpdate ()
 	{
+		// Get current position, update position and rotation.
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
